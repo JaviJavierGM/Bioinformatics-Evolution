d4a649479978bfdd487ba28e29b81a05b8049943
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2021-01-28 20:02:03 -0600

    froalaeditor-add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { routing, appRoutingProviders } from './app.routing';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -32,7 +33,9 @@ import { UsersManagementComponent } from './components/users-management/users-ma
     BrowserModule,
     routing,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot()
   ],
   providers: [
     appRoutingProviders
