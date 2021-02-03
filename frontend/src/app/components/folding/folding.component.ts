e2dde0891c47f2f64315b6b4c848ad6b0d5ce628
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2021-02-02 19:55:21 -0600

    edit-folding-component-user-edit

--- frontend/src/app/components/folding/folding.component.ts
+++ frontend/src/app/components/folding/folding.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { EvolutionaryAlgorithm } from '../../models/evolutionaryAlgorithm';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-folding',
   templateUrl: './folding.component.html',
-  styleUrls: ['./folding.component.css']
+  styleUrls: ['./folding.component.css'],
+  providers: [UserService]
 })
 export class FoldingComponent implements OnInit {
   public page_title: string;
   public evolutionaryAlgorithm: EvolutionaryAlgorithm;
+  public identity;
+  public token;
 
-  constructor() { 
+  constructor(
+    private _userService: UserService
+  ) { 
     this.page_title = 'Protein folding';
     this.evolutionaryAlgorithm = new EvolutionaryAlgorithm('', 'lattice', '2D_Square', 'simple', 'roulette', 'one_point', 'predefined', 100, 200, 1, 1, 20, 20, 1.0, 0.0, 0.01, 0.01);
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   ngOnInit(): void {
@@ -29,6 +37,11 @@ export class FoldingComponent implements OnInit {
     } else {
       return false;
     }
+  }
+
+  saveProject() {
+    console.log('Se guardara el proyecto en la DB');
+    console.log(this.evolutionaryAlgorithm);
   }
 
 }
