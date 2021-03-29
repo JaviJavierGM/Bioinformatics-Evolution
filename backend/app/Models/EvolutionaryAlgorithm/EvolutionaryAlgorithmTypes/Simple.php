8b729bc03d26208b4fa264ede63934df75722088
JaviJavierGM <ja.1999.ga@gmail.com>
2021-03-29 00:26:46 -0600

    simple finished

--- backend/app/Models/EvolutionaryAlgorithm/EvolutionaryAlgorithmTypes/Simple.php
+++ backend/app/Models/EvolutionaryAlgorithm/EvolutionaryAlgorithmTypes/Simple.php
@@ -93,165 +93,35 @@ class Simple extends EvolutionaryAlgorithm
     public function executeVersion1() {
         echo 'Esta es la version 1 xD la mmlona xD <br>';
 
-        // Generar  la generacion inicial
-        switch ($this->dimensionType) {
-            case '2D_Square':
-                if($this->spaceType == "correlated") {
-                    // Se generan los puntos 2D cuadrado en un medio correlacionado
-                    // ------- PENDIENTE ------- 
-                }else{
-                    // Se generan los puntos 2D cuadrado en un medio homogeno
-                    $generatePoints = new GenerateSquarePoints($this->hpSecuence, $this->spaceType, null, $this->dimensionType, $this->functionType, $this->alphaValue);
-                    $generation = $generatePoints->initializeGeneration($this->conformationsNumber);                    
-                }
-            break;
+        
 
-            case '2D_Triangle':
-                // Se generan los puntos 2D triangular en un medio homogeno
-                $generatePoints = new GenerateTrianglePoints($this->hpSecuence, $this->spaceType, null, $this->dimensionType, $this->functionType, $this->alphaValue);
-                $generation = $generatePoints->initializeGeneration($this->conformationsNumber);                
-            break;
+        for($numExp=0; $numExp < $this->experimentsNumber; $numExp++) {
 
-            case '3D_Cubic':
-                // Se generan los puntos 3D cubico en un medio homogeno
-                $generatePoints = new GenerateCubePoints($this->hpSecuence, $this->spaceType, null, $this->dimensionType, $this->functionType, $this->alphaValue);
-                $generation = $generatePoints->initializeGeneration($this->conformationsNumber);
-            break;
-            
-            default:
-                echo 'Default case';
-            break;
-        }
+            $j = 1;
 
-        echo "<br> PRIMERA GENERACION <br>";
-                    $cont=0;
-                    foreach($generation->getConformations() as $conformation){
-                        echo "Conformation ".$cont."<br>";
-                        $contPoint=0;
-                        foreach($conformation->getPoints() as $point){
-                            echo "Punto ".$contPoint."<br>";
-                            echo "X: ".$point->getValueX()." , Y: ".$point->getValueY()." , Z: ".$point->getValueZ()."<br>";
-                            // echo "movVect: ".$point->getMovVectorValue()."<br>";
-                            $contPoint++;
-                        }
-                        
-                        $cont++;
+            // Generar  la generacion inicial
+            switch ($this->dimensionType) {
+                case '2D_Square':
+                    if($this->spaceType == "correlated") {
+                        // Se generan los puntos 2D cuadrado en un medio correlacionado
+                        // ------- PENDIENTE ------- 
+                    }else{
+                        // Se generan los puntos 2D cuadrado en un medio homogeno
+                        $generatePoints = new GenerateSquarePoints($this->hpSecuence, $this->spaceType, null, $this->dimensionType, $this->functionType, $this->alphaValue);
+                        $generation = $generatePoints->initializeGeneration($this->conformationsNumber);                    
                     }
-
-        for($i=0; $i<$this->generationsNumber; $i++) {
-            // Seleccion de padres
-            if($this->elitismSelected) {
-                if($this->selectionOperator == 'tournament') {
-                    $elitism = new Elitism($this->percentOfElitism, $generation, $this->selectionOperator, $this->percentOfTournament);
-                    $elitism->execute();                    
-                } elseif($this->selectionOperator == 'top_percent') {
-                    $elitism = new Elitism($this->percentOfElitism, $generation, $this->selectionOperator, $this->percentOfTournament);
-                    $elitism->execute();
-                } else {
-                    $elitism = new Elitism($this->percentOfElitism, $generation, $this->selectionOperator, null);
-                    $elitism->execute();
-                }
-                                
-            } else {
-                switch ($this->selectionOperator) {
-                    case 'roulette':
-                        $roulette = new Roulette($generation);
-                        $roulette->execute();                        
-                    break;
-
-                    case 'tournament':
-                        $tournament = new Tournament($generation, $this->percentOfTournament);
-                        $tournament->execute();                        
-                    break;
-
-                    case 'top_percent':
-                        $topPercent = new TopPercent($generation, $this->percentOfTournament);
-                        $topPercent->execute();                        
-                    break;
-
-                    case 'population_decimation':
-                        $populationDecimation = new PopulationDecimation($generation);
-                        $populationDecimation->execute();                        
-                    break;
-                        
-                    default:
-                        echo 'Default case';
-                    break;
-                }
-            }
-
-            // Formacion de parejas
-            $coupleFormation = new SimplexCoupleFormation($generation);
-            $coupleFormation->coupleFormation();
-    
-            // Cruce
-            switch ($this->crossoverType) {
-                case 'one_point':
-                    $crossoverOnePoint = new OnePoint(
-                        $generation, 
-                        $this->spaceType, 
-                        $this->dimensionType, 
-                        strlen($this->hpSecuence), 
-                        // $this->conformationsNumber, 
-                        $this->crossoverProbability, 
-                        null, 
-                        $this->hpSecuence, 
-                        $this->mutationType,
-                        $this->functionType,
-                        $this->alphaValue
-                    );
-                    // var_dump($crossoverOnePoint->getNewGeneration()); die();
-                    array_push($this->currentExperiment, $crossoverOnePoint->getNewGeneration());
-                    var_dump($crossoverOnePoint->getNewGeneration()); die();
-                    echo "<br> NUEVA GENERACION <br>";
-                    $cont=0;
-                    foreach($crossoverOnePoint->getNewGeneration()->getConformations() as $conformation){
-                        echo "Conformation ".$cont."<br>";
-                        $contPoint=0;
-                        foreach($conformation->getPoints() as $point){
-                            echo "Punto ".$contPoint."<br>";
-                            echo "X: ".$point->getValueX()." , Y: ".$point->getValueY()." , Z: ".$point->getValueZ()."<br>";
-                            // echo "movVect: ".$point->getMovVectorValue()."<br>";
-                            $contPoint++;
-                        }
-                        
-                        $cont++;
-                    }
-                    die();
-
                 break;
 
-                case 'two_points':
-                    $crossoverTwoPoints = new TwoPoints(
-                        $generation, 
-                        $this->spaceType, 
-                        $this->dimensionType, 
-                        strlen($this->hpSecuence), 
-                        // $this->conformationsNumber, 
-                        $this->crossoverProbability, 
-                        null, 
-                        $this->hpSecuence, 
-                        $this->mutationType
-                    );
-                    // var_dump($crossoverTwoPoints->getNewGeneration()); die();
-                    array_push($this->currentExperiment, $crossoverTwoPoints->getNewGeneration());
+                case '2D_Triangle':
+                    // Se generan los puntos 2D triangular en un medio homogeno
+                    $generatePoints = new GenerateTrianglePoints($this->hpSecuence, $this->spaceType, null, $this->dimensionType, $this->functionType, $this->alphaValue);
+                    $generation = $generatePoints->initializeGeneration($this->conformationsNumber);                
                 break;
 
-                case 'uniform':
-                    $crossoverUniform = new Uniform(
-                        $generation, 
-                        $this->spaceType, 
-                        $this->dimensionType, 
-                        strlen($this->hpSecuence), 
-                        // $this->conformationsNumber, 
-                        $this->crossoverProbability, 
-                        null, 
-                        $this->hpSecuence, 
-                        $this->mutationType
-                    );
-                    // var_dump($crossoverUniform->getNewGeneration()->getConformations()); die();                    
-                    array_push($this->currentExperiment, $crossoverUniform->getNewGeneration());
-                    
+                case '3D_Cubic':
+                    // Se generan los puntos 3D cubico en un medio homogeno
+                    $generatePoints = new GenerateCubePoints($this->hpSecuence, $this->spaceType, null, $this->dimensionType, $this->functionType, $this->alphaValue);
+                    $generation = $generatePoints->initializeGeneration($this->conformationsNumber);
                 break;
                 
                 default:
@@ -259,84 +129,149 @@ class Simple extends EvolutionaryAlgorithm
                 break;
             }
 
+            // Agregamos la primera generacion            
+            array_push($this->currentExperiment, $generation);
+
+            // if($numExp == 1)
+            //     die();
+
+            for($i=0; $i<$this->generationsNumber; $i++) {
+                // Seleccion de padres
+                if($this->elitismSelected) {
+                    if($this->selectionOperator == 'tournament') {
+                        $elitism = new Elitism($this->percentOfElitism, $generation, $this->selectionOperator, $this->percentOfTournament);
+                        $elitism->execute();                    
+                    } elseif($this->selectionOperator == 'top_percent') {
+                        $elitism = new Elitism($this->percentOfElitism, $generation, $this->selectionOperator, $this->percentOfTournament);
+                        $elitism->execute();
+                    } else {
+                        $elitism = new Elitism($this->percentOfElitism, $generation, $this->selectionOperator, null);
+                        $elitism->execute();
+                    }
+                                    
+                } else {
+                    switch ($this->selectionOperator) {
+                        case 'roulette':
+                            $roulette = new Roulette($generation);
+                            $roulette->execute();                        
+                        break;
+
+                        case 'tournament':
+                            $tournament = new Tournament($generation, $this->percentOfTournament);
+                            $tournament->execute();                        
+                        break;
+
+                        case 'top_percent':
+                            $topPercent = new TopPercent($generation, $this->percentOfTournament);
+                            $topPercent->execute();                        
+                        break;
+
+                        case 'population_decimation':
+                            $populationDecimation = new PopulationDecimation($generation);
+                            $populationDecimation->execute();                        
+                        break;
+                            
+                        default:
+                            echo 'Default case';
+                        break;
+                    }
+                }
+
+                // Formacion de parejas
+                $coupleFormation = new SimplexCoupleFormation($generation);
+                $coupleFormation->coupleFormation();
+        
+                // Cruce, mutacion
+                switch ($this->crossoverType) {
+                    case 'one_point':
+                        $crossoverOnePoint = new OnePoint(
+                            $generation, 
+                            $this->spaceType, 
+                            $this->dimensionType, 
+                            strlen($this->hpSecuence), 
+                            $this->crossoverProbability, 
+                            null, 
+                            $this->hpSecuence, 
+                            $this->mutationType,
+                            $this->functionType,
+                            $this->alphaValue
+                        );
+                        array_push($this->currentExperiment, $crossoverOnePoint->getNewGeneration());
+                        $generation = $crossoverOnePoint->getNewGeneration();
+                    break;
+
+                    case 'two_points':
+                        $crossoverTwoPoints = new TwoPoints(
+                            $generation, 
+                            $this->spaceType, 
+                            $this->dimensionType, 
+                            strlen($this->hpSecuence), 
+                            $this->crossoverProbability, 
+                            null, 
+                            $this->hpSecuence, 
+                            $this->mutationType
+                        );
+                        array_push($this->currentExperiment, $crossoverTwoPoints->getNewGeneration());
+                        $generation = $crossoverOnePoint->getNewGeneration();
+                    break;
+
+                    case 'uniform':
+                        $crossoverUniform = new Uniform(
+                            $generation, 
+                            $this->spaceType, 
+                            $this->dimensionType, 
+                            strlen($this->hpSecuence),
+                            $this->crossoverProbability, 
+                            null, 
+                            $this->hpSecuence, 
+                            $this->mutationType
+                        );
+                        array_push($this->currentExperiment, $crossoverUniform->getNewGeneration());
+                        $generation = $crossoverOnePoint->getNewGeneration();
+                    break;
+                    
+                    default:
+                        echo 'Default case';
+                    break;
+                }
+
+                if(($i % $this->sampling) == 0) {
+                    for($k=$j; $k<$i; $k++) {
+                        $this->currentExperiment[$k] = null;
+                    }
+                    $j = $i + 1;
+                }
+
+            }
+
+            $b = sizeof($this->currentExperiment);
+            // var_dump($j);
+            for($k=$j; $k<$b-1; $k++) {
+                $this->currentExperiment[$k] = null;
+            }
+            // var_dump($this->currentExperiment);
+            
+            array_push($this->experiments, $this->currentExperiment);
+            // unset($this->currentExperiment);
+            $this->currentExperiment = array();
+            // var_dump($this->currentExperiment);
+
+            // echo "Termino exp: ".$numExp."<br>";
+            // die();
+
+
         }
 
-        /*
-        $j = 1;
-        for($i=0; $i < $this->generationsNumber; $i++) {
-            if ($this->selectionOperator == 'roulette') {
-                $roulette = new Roulette($generation);
-                if(!($roulette->execute())){
-                    return false;
-                }
-            } elseif($this->selectionOperator == 'tournament') {
-                $tournament = new Tournament($generation, $this->percent);
-                if(!($tournament->execute())) {
-                    return false;
-                }
-            } elseif($this->selectionOperator == 'top_percent') {
-                $topPercent = new TopPercent($generation, $this->percent);
-                if(!($topPercent->execute())) {
-                    return false;
-                }
-            } elseif($this->selectionOperator == 'population_decimation') {
-                $populationDec = new PopulationDecimation($generation);
-                if(!($populationDec->execute())) {
-                    return false;
-                }
-            }
+        echo "termino";
+        
+        var_dump($this->experiments);
 
-            $coupleFormation = new SimplexCoupleFormation($generation);
-            $coupleFormation->coupleFormation();
+        
 
-            if($crossoverType == 'one_point') {
-                array_push($this->currentExperiment, new OnePoint(
-                    $generation, 
-                    $spaceType, 
-                    $dimensionType, 
-                    $lengthHpString, 
-                    $conformationsNumber, 
-                    $crossoverProbability, 
-                    $correlatedMatrix, 
-                    $hpSecuence, 
-                    $mutationType
-                ));
-            } elseif($crossoverType == 'two_points') {
-                array_push($this->currentExperiment, new TwoPoints(
-                    $generation, 
-                    $spaceType, 
-                    $dimensionType, 
-                    $lengthHpString, 
-                    $conformationsNumber, 
-                    $crossoverProbability, 
-                    $correlatedMatrix, 
-                    $hpSecuence, 
-                    $mutationType
-                ));
-            } elseif($crossoverType == 'uniform') {
-                array_push($this->currentExperiment, new Uniform(
-                    $generation, 
-                    $spaceType, 
-                    $dimensionType, 
-                    $lengthHpString, 
-                    $conformationsNumber, 
-                    $crossoverProbability, 
-                    $correlatedMatrix, 
-                    $hpSecuence, 
-                    $mutationType
-                ));
-            }
+        die();
 
-            if(($i % $this->sampling) == 0) {
-                for ($k=$j; $k < $i; $k++) { 
-                    echo 'Se debe modificar el arreglo de experimentos !!!<br>';
-                }
-                $j = $i + 1;
-            }
-        }
-
-        $experimentsSize = sizeof($this->experiments);
-        */
+        return true;
+        
     }
 
     public function executeVersion2() {
