07a89e5c930a4b8cd2f44bab226c192deb637885
clanersitow <45682025+clanersitow@users.noreply.github.com>
2021-04-02 21:15:31 -0600

    install_grafica

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { routing, appRoutingProviders } from './app.routing';
 import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { AngularFileUploaderModule } from "angular-file-uploader";
+import {ChartsModule} from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -22,6 +23,8 @@ import { EngineComponent } from './components/engine/engine.component';
 import { Engine3DComponent } from './components/engine3-d/engine3-d.component';
 import { ResultsComponent } from './components/results/results.component';
 import { SelectCorrelatedNetworkComponent } from './components/select-correlated-network/select-correlated-network.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
+
 
 @NgModule({
   declarations: [
@@ -41,13 +44,15 @@ import { SelectCorrelatedNetworkComponent } from './components/select-correlated
 
     ResultsComponent,
 
-    SelectCorrelatedNetworkComponent
+    SelectCorrelatedNetworkComponent,
+      GraficaComponent
   ],
   imports: [
     BrowserModule,
     routing,
     FormsModule,
     HttpClientModule,
+    ChartsModule,
     FroalaEditorModule.forRoot(),
     FroalaViewModule.forRoot(),
     AngularFileUploaderModule,
