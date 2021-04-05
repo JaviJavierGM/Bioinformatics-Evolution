0fb3ff74dfb15f55ef75b1cf5bd73541e1d73c7a
clanersitow <45682025+clanersitow@users.noreply.github.com>
2021-04-05 01:33:24 -0500

    graph simple

--- frontend/src/app/components/results/results.component.ts
+++ frontend/src/app/components/results/results.component.ts
@@ -4,18 +4,40 @@ import { Folding } from '../../models/folding';
 import {EngineService} from '../../services/engine.service';
 import {Engine3DService} from '../../services/engine3-d.service'
 
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
+
 export class ResultsComponent implements OnInit {
   public page_title: string;
-  public experiments;
+  public experiments: any[][];
   public dimensionType: string;
   public spaceType: string;
   public folding: Folding;
   public plot: Folding;
+
+  public lineChartData: ChartDataSets[];
+  public lineChartLabels: Label[] ;
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+
 
   @ViewChild('rendererCanvas', {static: true})
   public rendererCanvas: ElementRef<HTMLCanvasElement>;
@@ -23,6 +45,7 @@ export class ResultsComponent implements OnInit {
   constructor(
     private engServ: EngineService, 
     private engServ3D: Engine3DService, 
+
     public resultsData: ResultsDataService
   ) {
     this.page_title = 'Results of the Evolutionary Algorithm!!';
@@ -35,10 +58,7 @@ export class ResultsComponent implements OnInit {
     this.experiments = this.resultsData.resultsExperiments;
     this.dimensionType = this.resultsData.dimensionType;
     this.spaceType = this.resultsData.spaceType;
-
-    
-  
-    
+       
     console.log('Componente de resultados!!');
     /* console.log(this.experiments);
     console.log(this.dimensionType);
@@ -46,7 +66,7 @@ export class ResultsComponent implements OnInit {
 
   }
 
-  onSubmit(form) {
+  onSubmit(form: any) {
     let generation = this.experiments[this.folding.experiment][this.folding.generation];
     let conformationClone = generation[0][this.folding.conformation];
 
@@ -67,14 +87,37 @@ export class ResultsComponent implements OnInit {
 
   graphSumFitness() {
     console.log('Aqui se hace la grafica de la suma de fitness de cada generacion!!');
+
+
   }
 
-  graphFitnessParticularGeneration(form) {
-    console.log('Aqui se hace la grafica el fitness de cada conformacion!!');
-    console.log(this.plot);
+  graphFitnessParticularGeneration(form: any) {
+   
+     /*  console.log('Aqui se hace la grafica el fitness de cada conformacion!!');
+    console.log(this.plot);  */
 
     let generation =  this.experiments[this.plot.experiment][this.plot.generation];
     console.log(generation);
+    console.log(generation[0][0].fitness);
+    let mydata =[];
+    generation[0].forEach(element => { mydata.push(element.fitness*-1);
+      
+    });
+    console.log(generation[0][0]);
+
+   /*  let data =[{number,number}];
+    for (let index = 0; index < generation.length; index++) {
+      let element = generation[index];
+      data.push({index,element})
+      
+    } */
+
+    this.lineChartData=[ { data: mydata, label: 'Series A' }  ];
+    
+
+
+    
+  
     
   }
 
