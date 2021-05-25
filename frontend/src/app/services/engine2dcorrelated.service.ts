261c2e7b9913379a156bf257b09c1d7f39b739d8
clanersitow <45682025+clanersitow@users.noreply.github.com>
2021-05-24 20:22:02 -0500

    update union correlated ready

--- frontend/src/app/services/engine2dcorrelated.service.ts
+++ frontend/src/app/services/engine2dcorrelated.service.ts
@@ -132,16 +132,22 @@ export class Engine2DCorrelatedService implements OnDestroy {
        
 
     //Add union
-    const points = []
-    for (var index = 0; index < array_C.points.length; index++) {
-      points.push(new THREE.Vector3(array_C.points[index].yValue*6, array_C.points[index].xValue*-6,array_C.points[index].zValue*6));
+
+    for(var conta=0; conta<array_C.points.length-1;conta++ ){
+
+      const points = []
+
+      for (var index = 0; index < 2; index++) {
+        points.push(new THREE.Vector3(array_C.points[conta+index].yValue*6, array_C.points[conta+index].xValue*-6,array_C.points[conta+index].zValue*6));
+
+      }
+      
+      var pathBase = new THREE.CatmullRomCurve3(points);
+      var tgeometry = new THREE.TubeGeometry( pathBase, array_C.points.length-1, .8, 20, false );
+      var tmaterial = new THREE.MeshBasicMaterial( { color: 0xCCCCCC } );
+      var tmesh = new THREE.Mesh( tgeometry, tmaterial );
+      this.scene.add(tmesh);
     }
-    var pathBase = new THREE.CatmullRomCurve3(points);
-    var tgeometry = new THREE.TubeGeometry( pathBase, array_C.points.length-1, .8, 20, false );
-    var tmaterial = new THREE.MeshBasicMaterial( { color: 0xCCCCCC } );
-    var tmesh = new THREE.Mesh( tgeometry, tmaterial );
-    this.scene.add(tmesh);
-     
 
     //Add mesh
 
